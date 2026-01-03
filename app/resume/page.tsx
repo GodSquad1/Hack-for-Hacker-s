3bf1086199331180ebaca3f10e1686c865a1abbb
savirvenkatraman28 <savirvenkatraman28@mittymonarch.com>
2026-01-02 19:18:15 -0800

    Add ResumeAnalyzer and Footer components to ResumePage

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,1 +1,12 @@
+import { ResumeAnalyzer } from "@/components/resume-analyzer"
+import { Footer } from "@/components/footer"
+
+export default function ResumePage() {
+  return (
+    <div className="min-h-screen dark">
+      <ResumeAnalyzer />
+      <Footer />
+    </div>
+  )
+}
 
