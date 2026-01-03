a172678fe4d0265f139cbf5879b0ec94a201e3d4
savirvenkatraman28 <savirvenkatraman28@mittymonarch.com>
2026-01-02 19:21:50 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,1 @@
-import { TrendsDashboard } from "@/components/trends-dashboard"
-import { Footer } from "@/components/footer"
-
-export default function TrendsPage() {
-  return (
-    <div className="min-h-screen dark">
-      <TrendsDashboard />
-      <Footer />
-    </div>
-  )
-}
 
