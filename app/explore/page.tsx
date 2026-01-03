8ece62383618b7cba65de59ef1e521620bb7fbb8
savirvenkatraman28 <savirvenkatraman28@mittymonarch.com>
2026-01-02 19:14:51 -0800

    Add InternshipExplorer and Footer components

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,1 +1,12 @@
+import { InternshipExplorer } from "@/components/internship-explorer"
+import { Footer } from "@/components/footer"
+
+export default function ExplorePage() {
+  return (
+    <div className="min-h-screen dark">
+      <InternshipExplorer />
+      <Footer />
+    </div>
+  )
+}
 
